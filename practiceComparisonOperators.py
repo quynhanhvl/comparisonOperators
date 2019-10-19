8d577b65bc4a725b861bb68af1be443786296a89
quynhanhvl <quynhanhvl@users.noreply.github.com>
2019-10-19 10:27:26 -0500

    Initial commit

---
+++ practiceComparisonOperators.py
@@ -0,0 +1,32 @@
+'''
+Created on Oct 5, 2019
+
+@author: ITAUser
+'''
+
+'''=='''
+print(4==4)
+print(4==2)
+
+''' > or < '''
+print(2>3)
+print(3>2)
+
+''' => or <= '''
+print(4>=4)
+print(5>=4)
+print(4<=4)
+print(5<=4)
+
+harry = (4==5)
+print("Result: " + str(harry))
+
+''' != '''
+#not equal
+
+
+
+
+
+
+
